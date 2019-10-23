e3758816f607658eec366fa3afa513ee5d4d8da9
sunny <sunnypurewal@gmail.com>
2019-10-22 20:47:34 -0400

    A little implementation of htmlhandler

--- src/crawler/htmlhandler.ts
+++ src/crawler/htmlhandler.ts
@@ -1,7 +1,18 @@
 
+interface HTMLHandlerResult {
+  success: boolean
+  body: any
+}
+
+type onHTMLHandledCallback = (url: URL, result: HTMLHandlerResult) => void
+
 //implement HTMLHandler
 //then add it to crawler.htmlHandlers as default handler
 //then call all handlers after urlhandler is called
 export default class HTMLHandler {
-
+  handle(url: URL, onHTMLHandled: onHTMLHandledCallback) {
+    process.nextTick(() => {
+      onHTMLHandled(urlcopy, html)
+    })
+  }
 }
