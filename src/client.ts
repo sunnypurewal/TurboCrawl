2fb04bb57e2c8c9af9782555dc7ea723c35716ca
sunny <sunnypurewal@gmail.com>
2019-10-28 22:01:04 -0400

    Finished client

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 
 import crawl, { random } from "./commands/crawl"
 import { list } from "./commands/get"
+import { end, endall, pause, pauseall, resume, resumeall } from "./commands/manage"
 import crawlNational from "./commands/news"
 
 export default class Client {
@@ -26,5 +27,25 @@ export default class Client {
 
   public listCrawlers(callback: (crawlers: any) => void) {
     list(this.port, this.host, callback)
+  }
+
+  public end(urls: URL[], callback: (success: boolean) => void) {
+    end(this.port, this.host, urls, callback)
+  }
+  public pause(urls: URL[], callback: (success: boolean, err?: Error) => void) {
+    pause(this.port, this.host, urls, callback)
+  }
+  public resume(urls: URL[], callback: (success: boolean, err?: Error) => void) {
+    resume(this.port, this.host, urls, callback)
+  }
+
+  public endall(callback: (success: boolean, err?: Error) => void) {
+    endall(this.port, this.host, callback)
+  }
+  public pauseall(callback: (success: boolean, err?: Error) => void) {
+    pauseall(this.port, this.host, callback)
+  }
+  public resumeall(callback: (success: boolean, err?: Error) => void) {
+    resumeall(this.port, this.host, callback)
   }
 }
---
+++ src/commands/manage.ts
@@ -0,0 +1,72 @@
+import { request } from "http"
+
+export function pause(port: number, host: string, urls: URL[], callback: (success: boolean, err?: Error) => void) {
+  posturls(port, host, "/pause", urls, callback)
+}
+export function pauseall(port: number, host: string, callback: (success: boolean, err?: Error) => void) {
+  post(port, host, "/pauseall", callback)
+}
+export function end(port: number, host: string, urls: URL[], callback: (success: boolean, err?: Error) => void) {
+  posturls(port, host, "/end", urls, callback)
+}
+export function endall(port: number, host: string, callback: (success: boolean, err?: Error) => void) {
+  post(port, host, "/endall", callback)
+}
+export function resume(port: number, host: string, urls: URL[], callback: (success: boolean, err?: Error) => void) {
+  posturls(port, host, "/resume", urls, callback)
+}
+export function resumeall(port: number, host: string, callback: (success: boolean, err?: Error) => void) {
+  post(port, host, "/resumeall", callback)
+}
+
+function post(port: number, host: string, path: string, callback: (success: boolean, err?: Error) => void) {
+  const req = request({
+    headers: {"content-type": "application/json"},
+    host,
+    method: "POST",
+    path,
+    port,
+  }, (res) => {
+    let body: any = []
+    res.on("error", (err) => {
+      console.error(err)
+    }).on("data", (chunk) => {
+      body.push(chunk)
+    }).on("end", () => {
+      body = Buffer.concat(body).toString()
+      if (res.statusCode! >= 200 && res.statusCode! <= 299) {
+        process.nextTick(() => callback(true))
+      } else {
+        process.nextTick(() => callback(false, new Error("HTTP Error" + res.statusCode)))
+      }
+    })
+  })
+  req.end()
+}
+
+function posturls(port: number, host: string, path: string,
+                  urls: URL[], callback: (success: boolean, err?: Error) => void) {
+  const req = request({
+    headers: {"content-type": "application/json"},
+    host,
+    method: "POST",
+    path,
+    port,
+  }, (res) => {
+    let body: any = []
+    res.on("error", (err) => {
+      console.error(err)
+    }).on("data", (chunk) => {
+      body.push(chunk)
+    }).on("end", () => {
+      body = Buffer.concat(body).toString()
+      if (res.statusCode! >= 200 && res.statusCode! <= 299) {
+        process.nextTick(() => callback(true))
+      } else {
+        process.nextTick(() => callback(false, new Error("HTTP Error" + res.statusCode)))
+      }
+    })
+  })
+  req.write(JSON.stringify(urls.map((u) => u.href)))
+  req.end()
+}
